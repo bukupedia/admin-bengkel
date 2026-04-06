4f9ee4bb232c8f377eef6547ab18f34a9bc8c280
Arif Budiman <arifbudiman@outlook.com>
2026-04-06 22:38:19 +0700

    Refactor navbar rendering with updated structure

--- assets/js/components/navbar.js
+++ assets/js/components/navbar.js
@@ -1,13 +1,44 @@
+// assets/js/components/navbar.js
+
 export function renderNavbar() {
-  document.getElementById("navbar").innerHTML = `
+  const navbar = document.getElementById("navbar");
+
+  if (!navbar) return;
+
+  navbar.innerHTML = `
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="dashboard.html">BengkelApp</a>
-        <div>
-          <a href="servis.html" class="btn btn-sm btn-light">Servis</a>
-          <a href="pelanggan.html" class="btn btn-sm btn-light">Pelanggan</a>
-          <a href="sparepart.html" class="btn btn-sm btn-light">Sparepart</a>
+        
+        <a class="navbar-brand fw-bold" href="dashboard.html">
+          BengkelApp
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navMenu">
+          <ul class="navbar-nav ms-auto">
+
+            <li class="nav-item">
+              <a class="nav-link" href="dashboard.html">Dashboard</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="servis.html">Servis</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="pelanggan.html">Pelanggan</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="sparepart.html">Sparepart</a>
+            </li>
+
+          </ul>
         </div>
+
       </div>
     </nav>
   `;
